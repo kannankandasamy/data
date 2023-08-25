2e140266cf7d71eb4bbcbffe84bdb9ae004666c7
Kannan Kandasamy <kannanvijay@hotmail.com>
2023-08-24 21:57:12 -0400

    changed query in snowflake and added line chart

--- streamlit101/st_snowflake.py
+++ streamlit101/st_snowflake.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.header("Get data from Snowflake")
 
 conn = st.experimental_connection('snowpark')
-df = conn.query("select * from dim_customer;", ttl=600)
+df = conn.query("""select product_id, sum(total_amount) as Amount  from sales_fact 
+                group by product_id;""", ttl=600)
 
 st.write(df)
+
+st.subheader("Chart below")
+st.line_chart(df)
